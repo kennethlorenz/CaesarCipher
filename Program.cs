30595add02a976fa212af7d9424e554bf3ec1fbb
Kenneth <kennethlorenzg@gmail.com>
2021-09-12 14:56:59 -0400

    added charToEncrypt method

--- Program.cs
+++ Program.cs
@@ -4,9 +4,24 @@ namespace CaesarCipher
 {
     class Program
     {
+        //this method shifts the original character to a specific character based on 
+        //the key given by the user.
+        public static char charToEncrypt(char c, int key)
+        {
+            //check if the character is a number
+            //if it is a number, we return it, if not, we shift the char based on key specified.
+            if (char.IsNumber(c))
+            {
+                return c;
+            }
+
+            return c;
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");asd
+            Console.WriteLine("Enter a string to encrypt: ");
+            string stringToEncrypt = Console.ReadLine();
+
         }
     }
 }
