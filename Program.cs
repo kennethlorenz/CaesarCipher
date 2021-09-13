04ab4507d25405287c15049123ecaa8ace49f7d9
Kenneth <kennethlorenzg@gmail.com>
2021-09-13 19:12:58 -0400

    Fixed CharToEncrypt method & Added Encrypt method

--- Program.cs
+++ Program.cs
@@ -6,33 +6,66 @@ namespace CaesarCipher
     {
         //this method shifts the original character to a specific character based on 
         //the key given by the user.
-        public static char charToEncrypt(char c, int key)
+        public static char CharToEncrypt(char c, int key)
         {
             //check if the character is a number
             //if it is a number, we return it, if not, we shift the char based on key specified.
-            if (char.IsNumber(c))
+            //We can use this if statement. However, it will return special characters for white spaces etc.
+            //if (!char.IsNumber(c))
+            //{
+            //    return c;
+            //}
+
+
+            //check if the character is not a letter
+            //if it's not a letter, we return it, if it is, we return & shift the char based on key specified.
+            if (!char.IsLetter(c))
             {
                 return c;
             }
 
 
-            return (char)(c+key);
+            //we use the modulus operator to get the remainder of the key
+            //caesar cipher is from 0-25, so if the user enters 26 as the key, the designated output resets respectively.
+            return (char)(c + (key % 26));
+        }
+
+
+        //this method encrypts the input and combines every single character in one whole string.
+        public static string Encrypt(string stringToEncrypt, int key)
+        {
+            string output = string.Empty;
+
+            //loop through the entire string to get each character
+            //and use the CharToEncrypt method to encrypt the letters based on the key given by the user.
+            foreach (char c in stringToEncrypt)
+            {
+                output += CharToEncrypt(c, key);
+            }
+
+            return output;
+        }
+
+        //this method decrypts the encrypted message
+        public static string Decrypt(string stringToDecrypt, int key)
+        {
+
         }
 
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a string to encrypt: ");
+            Console.Write("Enter a string to encrypt: ");
             string stringToEncrypt = Console.ReadLine();
 
-            string output = string.Empty;
+            Console.Write("Enter key: ");
+            int key = int.Parse(Console.ReadLine());
 
-            foreach(char c in stringToEncrypt)
-            {
-                output += charToEncrypt(c, 1);
-            }
+            string encryptedMessage = Encrypt(stringToEncrypt, key);
 
-            Console.WriteLine($"{output}");
+            Console.WriteLine($"Encrypted Message: {encryptedMessage}");
+
+
 
         }
     }
